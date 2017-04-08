2f1701a148a6220a5f908502df2393f88ba67638
Piotr Wittchen <piotr@wittchen.biz.pl>
2017-04-08 12:48:16 +0200

    fixing install script

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 set -e
-wget -O ydownloader https://raw.githubusercontent.com/pwittchen/tmux-auto-pane/master/tmux-auto-pane
+wget -O tmux-auto-pane https://raw.githubusercontent.com/pwittchen/tmux-auto-pane/master/tmux-auto-pane
 chmod +x tmux-auto-pane
 sudo mv tmux-auto-pane /usr/local/bin/tmux-auto-pane
